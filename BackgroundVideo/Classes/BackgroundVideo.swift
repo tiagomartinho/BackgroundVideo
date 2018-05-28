d000bfe9cfcd8ef8ea3f3bab1a2fa9ae7911b417
Tiago Martinho <tiago@elit.software>
2018-05-28 08:46:01 +0200

    Use auto layout

--- BackgroundVideo/Classes/BackgroundVideo.swift
+++ BackgroundVideo/Classes/BackgroundVideo.swift
@@ -8,8 +8,32 @@ extension UIView {
         let url = URL(fileURLWithPath: path)
         let player = AVPlayer(url: url)
         let playerLayer = AVPlayerLayer(player: player)
-        layer.addSublayer(playerLayer)
-        playerLayer.frame = bounds
+        let playerView = PlayerView()
+        playerView.playerLayer = playerLayer
+        playerLayer.videoGravity = .resizeAspectFill
+        playerView.layer.addSublayer(playerLayer)
+
+        addSubview(playerView)
+
+        playerView.translatesAutoresizingMaskIntoConstraints = false
+        if #available(iOS 9.0, *) {
+            playerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            playerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+            playerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            playerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        }
+
+
         player.play()
+    }
+}
+
+class PlayerView: UIView {
+
+    var playerLayer: CALayer?
+
+    override func layoutSublayers(of layer: CALayer) {
+        super.layoutSublayers(of: layer)
+        playerLayer?.frame = bounds
     }
 }
