8c18410d1089b3439c470eaf318e167193127446
Tiago Martinho <tiago@elit.software>
2018-05-28 08:25:39 +0200

    First working PoC

--- BackgroundVideo/Classes/BackgroundVideo.swift
+++ BackgroundVideo/Classes/BackgroundVideo.swift
@@ -1,4 +1,15 @@
+import AVFoundation
+
 extension UIView {
     public func add(video: String) {
+        guard let path = Bundle.main.path(forResource: "Video", ofType:"mp4") else {
+            return
+        }
+        let url = URL(fileURLWithPath: path)
+        let player = AVPlayer(url: url)
+        let playerLayer = AVPlayerLayer(player: player)
+        layer.addSublayer(playerLayer)
+        playerLayer.frame = bounds
+        player.play()
     }
 }
