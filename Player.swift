ff923c04767f98c1af7eef93e335387da1489896
Tiago Martinho <martinho@thoughtworks.com>
2018-05-29 16:18:39 +0200

    Play/Pause when app goes into Foreground/Bakcground

--- Player.swift
+++ Player.swift
@@ -2,19 +2,34 @@ import AVFoundation
 
 public class Player {
 
-    let playerLooper: AVPlayerLooper
-    let player: AVPlayer
-
-    init(playerLooper: AVPlayerLooper, player: AVPlayer) {
-        self.playerLooper = playerLooper
-        self.player = player
-    }
-
     public func play() {
         player.play()
     }
 
     public func pause() {
         player.pause()
+    }
+
+    private let playerLooper: AVPlayerLooper
+    private let player: AVPlayer
+    private let notificationCenter = NotificationCenter.default
+
+    init(playerLooper: AVPlayerLooper, player: AVPlayer) {
+        self.playerLooper = playerLooper
+        self.player = player
+        notificationCenter.addObserver(self, selector: #selector(applicationWillResignActive), name: Notification.Name.UIApplicationWillResignActive, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(applicationWillEnterForeground), name: Notification.Name.UIApplicationWillEnterForeground, object: nil)
+    }
+
+    deinit {
+        notificationCenter.removeObserver(self)
+    }
+
+    @objc private func applicationWillResignActive() {
+        pause()
+    }
+
+    @objc private func applicationWillEnterForeground() {
+        play()
     }
 }
