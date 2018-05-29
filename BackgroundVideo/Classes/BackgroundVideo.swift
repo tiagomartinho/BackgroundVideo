d6184a2dfb16979ac2c1032a744d80e175406143
Tiago Martinho <tiago@elit.software>
2018-05-29 07:50:10 +0200

    Extract add function

--- BackgroundVideo/Classes/BackgroundVideo.swift
+++ BackgroundVideo/Classes/BackgroundVideo.swift
@@ -9,22 +9,27 @@ extension UIView {
         let asset = AVAsset(url: videoURL)
         let item = AVPlayerItem(asset: asset)
         let player = AVQueuePlayer(playerItem: item)
-        let videoLooper = AVPlayerLooper(player: player, templateItem: item)
         let playerLayer = AVPlayerLayer(player: player)
         let playerView = PlayerView()
         playerView.playerLayer = playerLayer
         playerLayer.videoGravity = .resizeAspectFill
         playerView.layer.addSublayer(playerLayer)
 
+        add(playerView)
+        
+        player.play()
+
+        let videoLooper = AVPlayerLooper(player: player, templateItem: item)
+        return videoLooper
+    }
+
+    func add(_ playerView: UIView) {
         addSubview(playerView)
         playerView.translatesAutoresizingMaskIntoConstraints = false
         playerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
         playerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         playerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         playerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
-
-        player.play()
-        return videoLooper
     }
 }
 
